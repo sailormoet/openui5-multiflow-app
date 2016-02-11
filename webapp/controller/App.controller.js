bb9e1874698fcb1d42ad241e1ac5ee9ce5dbc76b
Bernadeth <perch_dhe2t@hotmail.com>
2016-02-11 21:19:05 +0100

    chore: add i18n support

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,17 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/resource/ResourceModel"
+], function (Controller, ResourceModel) {
   "use strict";
 
-  return Controller.extend("sm.multiflow.controller.App", {});
+  return Controller.extend("sm.multiflow.controller.App", {
 
+    onInit: function() {
+      var i18nModel = new ResourceModel({
+        bundleName: "sm.multiflow.i18n.i18n"
+      });
+      this.getView().setModel(i18nModel, "i18n");
+    }
+
+  });
 });
